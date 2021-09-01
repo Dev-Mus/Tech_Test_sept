562a7210a809ddba700126ab73c522f5bbbd4a54
Ouali Mustapha <haithamkanuaa3@gmail.com>
2021-09-01 17:03:18 +0100

    solution of test 2

--- 2nd_test/interviewApiNode/src/handlers.ts
+++ 2nd_test/interviewApiNode/src/handlers.ts
@@ -1,74 +1,104 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { PokemonWithStats } from "models/PokemonWithStats";
+// import { PokemonWithStats } from "models/PokemonWithStats";
 
-export async function getPokemonByName(request: FastifyRequest, reply: FastifyReply) {
-  var name: string = request.params['name']
-
-  reply.headers['Accept'] = 'application/json'
-
+export async function getPokemonByName(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
   var urlApiPokeman = `https://pokeapi.co/api/v2/pokemon/`;
+  reply.headers["Accept"] = "application/json";
 
-  var params = {}
+  var name: string = request.params["name"];
 
-  name == null
-      ? name.trim() != ''
-      ? (params["name"] = name, urlApiPokeman = urlApiPokeman + '/', urlApiPokeman = urlApiPokeman + name)
-      : (urlApiPokeman = urlApiPokeman + '"?offset=20"', urlApiPokeman = urlApiPokeman + "&limit=20")
-      : (urlApiPokeman = urlApiPokeman + '"?offset=20"', urlApiPokeman = urlApiPokeman + "&limit=20")
+  name != null && name.trim() != ""
+    ? (urlApiPokeman = urlApiPokeman + name)
+    : (urlApiPokeman = urlApiPokeman + "?offset=20" + "&limit=20");
 
-  const http = require('http');
-  const keepAliveAgent = new http.Agent({ keepAlive: true });
+  var response: any = "";
 
-  let response: any = ""
+  const axios = require("axios").default;
+  await axios
+    .get(urlApiPokeman)
+    .then(function (res) {
+      //? handle response
+      response = res.data;
+    })
+    .catch(function (error) {
+      //? handle error
+      console.log("error => ", error);
+      reply.code(404).send({
+        message: `name (${name}) is undefined !!`,
+      });
+    });
 
-  http.request({ ...reply.headers, ...({ hostname: urlApiPokeman, port: 80, }) }, (result) => { response = result })
+  var data = await computeResponse(response, reply);
 
-  if (response == null) {
-    reply.code(404)
-  }
+  console.log("\n\n\n Done ...");
 
-  computeResponse(response, reply)
-
-  reply.send(response)
-
-  return reply
+  reply.send(data);
+  return reply;
 }
 
 export const computeResponse = async (response: any, reply: FastifyReply) => {
-  const resp = response as any
+  const resp = response as any;
+  //? reducee type of current pokemon
+  let types = resp.types.map((type) => type.type.url);
 
-  let types = resp.types.map(type => type.type).map(type => { return type.url }).reduce((types, typeUrl) => types.push(typeUrl));
+  let pokemonTypes = [];
+  const axios = require("axios").default;
 
-  let pokemonTypes = []
+  //? Get data all pokemon of types
+  for (let i = 0; i < types.length; i++) {
+    const typeUrl = types[i];
+    await axios
+      .get(typeUrl)
+      .then(function (res) {
+        //? handle response
+        pokemonTypes = [...pokemonTypes, ...res.data.pokemon];
+      })
+      .catch(function (error) {
+        //? handle error
+        console.log("error => get pokemonTypes");
+      });
+  }
 
-  types.forEach(element => {
-    const http = require('http');
-    const keepAliveAgent = new http.Agent({ keepAlive: true });
+  if (pokemonTypes == undefined) throw pokemonTypes;
 
-    http.request({ hostname: element }, (response) => pokemonTypes.push(response))
+  //! lorsque je n'est pas lattrebuit stats => j'ai fait des appel vers api pour recupire data de pokemon
 
-  });
+  //? Get data of pokemon
+  var pokemonTypesWithStats = [];
+  for (let index = 0; index < pokemonTypes.length /*- 185 */; index++) {
+    const pokemonObj = pokemonTypes[index];
+    await axios
+      .get(pokemonObj.pokemon.url)
+      .then(function (res) {
+        //? handle response
+        pokemonTypesWithStats.push(res.data);
+      })
+      .catch(function (error) {
+        //? handle error
+        console.log("error => get pokemonTypesWithStats ");
+      });
+  }
 
-  if (pokemonTypes == undefined)
-    throw pokemonTypes
-
-  response.stats.forEach(element => {
-    var stats = []
-
-    pokemonTypes.map(pok =>
-        pok.stats.map(st =>
-            st.stat.name.toUpperCase() == element.stat.name
-                ? stats.push(st.base_state)
-                : ([])
-        )
-    )
-
+  //? get all base_stats
+  response.stats.map((element) => {
+    var stats = [];
+    pokemonTypesWithStats.map((pok) =>
+      pok.stats.map(
+        (st) =>
+          st.stat.name.toUpperCase() == element.stat.name.toUpperCase() &&
+          stats.push(st.base_stat)
+      )
+    );
+    //? average of base stats and add attribute averageStat too state
     if (stats) {
-      let avg = stats.reduce((a, b) => a + b) / stats.length
-      element.averageStat = avg
+      let avg = stats.reduce((a, b) => a + b) / stats.length;
+      element.averageStat = avg;
     } else {
-      element.averageStat = 0
+      element.averageStat = 0;
     }
   });
-
-}
+  return response;
+};
